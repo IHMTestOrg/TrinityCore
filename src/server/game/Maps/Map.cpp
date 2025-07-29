e13879df000f74cdff11053e5ad9e7986d6be033
ulmetrs <robertulmet@gmail.com>
2025-07-28 21:48:54 -0400

    Update Map.cpp

--- src/server/game/Maps/Map.cpp
+++ src/server/game/Maps/Map.cpp
@@ -599,7 +599,6 @@ bool Map::AddToMap(T* obj)
     /// @todo Needs clean up. An object should not be added to map twice.
     if (obj->IsInWorld())
     {
-        TC_LOG_ERROR("maps", "Map::AddToMap called on Object that is already in world, map {}, obj {}", GetId(), obj->GetDebugInfo());
         ASSERT(obj->IsInGrid());
         obj->UpdateObjectVisibility(true);
         return true;
