d97179e8c6306abad45fa6301f2debc3399e0437
ulmetrs <robertulmet@gmail.com>
2025-07-30 12:38:31 -0400

    Revert "Update Player.h"
    This reverts commit 811ed0bddde4f04d53d8e48399222a2949c499e9.

--- src/server/game/Entities/Player/Player.h
+++ src/server/game/Entities/Player/Player.h
@@ -957,7 +957,6 @@ class TC_GAME_API Player : public Unit, public GridObject<Player>
         bool Create(ObjectGuid::LowType guidlow, CharacterCreateInfo* createInfo);
 
         void Update(uint32 time) override;
-        void ProcessRelocation(uint32 diff) override;
 
         static bool BuildEnumData(PreparedQueryResult result, WorldPacket* data);
 
