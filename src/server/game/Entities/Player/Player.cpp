b70e80232cb263905d1ddba5212ad0ff42b84c51
ulmetrs <robertulmet@gmail.com>
2025-07-30 18:44:52 -0400

    Revert "Update Player.cpp"
    This reverts commit f3640c55a6cabb236b73e5695d64c21e7a408a26.

--- src/server/game/Entities/Player/Player.cpp
+++ src/server/game/Entities/Player/Player.cpp
@@ -1448,7 +1448,6 @@ void Player::ProcessVisibilityNotifier()
     ZoneScopedN("PlayerRelocationNotifier");
 
     PlayerRelocationNotifier relocate(*this);
-    WorldObject const* viewPoint = m_seer;
     Cell::VisitAllObjects(viewPoint, relocate, GetMap()->GetVisibilityRange(), false);
     relocate.SendToSelf();
 
